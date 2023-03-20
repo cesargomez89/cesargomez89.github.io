1f95d969a1468827b1938435879549a7f843ac7f
Cesar Gomez <cesargomez89@gmail.com>
2023-03-20 13:30:19 -0600

    same padding for skills and contact

--- app/about/about.js
+++ app/about/about.js
@@ -29,15 +29,13 @@ export default function About(){
       })
   }, [])
 
-  if (!aboutMe || !skills || !contactInfo) return <p>No profile data</p>
-
   return (
     <div id="about" className="min-h-screen bg-base-100 text-base-content grid grid-cols-2 max-sm:grid-cols-1 justify-items-center content-center p-2">
       <div className='max-w-screen-md text-left'>
         <h3 className="text-4xl max-md:text-2xl font-title font-bold">About me</h3>
         {
-          aboutMe.map(({paragraph, index})=> {
-            return <p key={index} className="py-2">{paragraph}</p>
+          aboutMe && aboutMe.map(({paragraph})=> {
+            return <p key={paragraph} className="py-2">{paragraph}</p>
           })
         }
       </div>
@@ -45,11 +43,11 @@ export default function About(){
         <h3 className="text-4xl max-md:text-2xl font-title font-bold px-4">Tech Stack</h3>
         <div className="grid grid-cols-4 max-lg:grid-cols-2 justify-between px-4 text-xl max-md:text-base">
           {
-            skills.map(({category, skills}, index)=>{
-              return <div key={index} className="flex flex-col p-4 max-md:p-2">
+            skills && skills.map(({category, skills})=>{
+              return <div key={category} className="flex flex-col p-4 max-md:p-2">
                 <span className="font-bold text-primary">{category}</span>
                 {
-                  skills.map((skill, index)=> <span key={index}>{skill}</span> )
+                  skills.map((skill)=> <span key={skill}>{skill}</span> )
                 }
               </div>
             })
@@ -60,10 +58,10 @@ export default function About(){
             <div className="grid grid-cols-2 py-2 max-lg:text-sm text-xl">
               <h3 className="text-4xl max-md:text-2xl font-title font-bold col-span-2 pb-2">Contact Info</h3>
               {
-                contactInfo.map(({title, content, url})=>{
+                contactInfo && contactInfo.map(({title, content, url})=>{
 
                   return <Fragment key={title}>
-                    <span>{title}:</span>
+                    <span className="pl-4 max-md:pl-2">{title}:</span>
                     <a className={url && "underline underline-offset-4"} href={url && "/resume-cesar-gomez.pdf"}>{content}</a>
                   </Fragment>
                 })
